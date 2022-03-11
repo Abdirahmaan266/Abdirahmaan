4ec3decd3e28a15998059b4c7deeac9e0b1f01da
Abdirahmaan266 <100992453+Abdirahmaan266@users.noreply.github.com>
2022-03-11 19:07:48 +0300

    Login Form

---
+++ Login form/Sltyle.cs
@@ -0,0 +1,7 @@
+.hero{
+    height: 100%;
+    width: 100%;
+    background-image: linear-gradiant (rbga(0,0,0,4), rgba(0,0,0,4)),url(banner.jbg);
+    background-size: cover;
+    possition: absolute;
+}
